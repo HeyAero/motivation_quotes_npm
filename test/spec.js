5587134f42e8c7eb93c571b928e9d0f2cd21da03
HeyAero <aeroyts@icloud.com>
2021-03-02 17:11:50 +0000

    add base test case to check if quote is given

--- test/spec.js
+++ test/spec.js
@@ -1,1 +1,12 @@
 // some code
+const motiQuote = require('../src/motivationQuote');
+
+describe('moti quote', () => {
+  describe('provide quote', () => {
+    let giveQuote = motiQuote.giveQuote;
+
+    test('it returns a motivational quote', () => {
+      expect(giveQuote().toExpect('keep going'));
+    });
+  });
+});
